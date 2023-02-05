a23a0a0ff3a05d5941ea3fc007e6e7afa20da57c
yutrack <YYO90001322@trac.astra.co.id>
2023-02-06 00:17:10 +0800

    add interface

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,7 +1,12 @@
+import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => (
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
   <div className="min-h-screen">
     <Navbar />
     <div>{children}</div>
